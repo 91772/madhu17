ae6750b459b4c1ddf89d5e41e49566d7e7ef8ff6
midhh <midhh@LAPTOP-PGSLOUCD>
2021-05-21 16:47:33 +0530

    my first commit

---
+++ core_java1/src/main/java/com/Multiplying2num.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Multiplying2num {
+
+	public static void main(String[] args) {
+		int i=7;
+		int j=6;
+		int k=i*j;
+		
+		System.out.println("Multiplication of  "+i+"  and  "+j+"  is :  "+k);
+
+
+	}
+
+}
